c0a0250d6ed3b1c87c6847399f47ef0a2038be8c
unknown <arthurguilherme2013@gmail.com>
2023-01-13 06:28:49 +0000

    Lessons 2 and 3

---
+++ variables_and_operators/02_lesson/js/script.js
@@ -0,0 +1,15 @@
+let num1 = prompt("Digit a number");
+let num2 = prompt("Digit other number");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+let sum = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+
+alert("sum is: " + sum);
+alert("subtraction is: " + subtraction);
+alert("multiplication is: " + multiplication);
+alert("division is: " + division);
---
+++ variables_and_operators/03_lesson/js/script.js
@@ -0,0 +1,9 @@
+let num1 = prompt("Digit a number");
+let num2 = prompt("Digit other number");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+let media = (num1 + num2) / 2;
+
+alert("You media is: " + media)
