256bffeb80eb5ff00406c59fc41d7d9d2b3f26b0
unknown <arthurguilherme2013@gmail.com>
2023-02-22 07:47:32 +0000

    logic of Programming - Studying Quicksort - p.115

---
+++ Logic_and_Algorithm/04_quicksort/02_recursive_sum/script.js
@@ -0,0 +1,21 @@
+function sum(list) {
+    if (list.length === 0) {
+        return 0;
+    } else {
+        return list[0] + sum(list.slice(1));
+    }
+}
+
+console.log(sum([1, 2, 3, 4]));
+
+/*
+p 115
+slice method: It basicly catch a initial index (for parameter) 
+and creates a new array "shadow", but final index not is included
+*/
+
+console.log("Learning Slice");
+const example = ["Arthur", "Guilherme", "Souza", "Silva"];
+const exampleWithSlice = example.slice(1);
+console.log("Example With Slice");
+console.log(exampleWithSlice);
